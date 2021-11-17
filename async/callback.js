f01b69343c054b109cba639e12fdba506eeabd6c
문규찬 <lazychan90@gmail.com>
2021-11-17 15:05:04 +0900

    call back hell!

---
+++ async/callback.js
@@ -0,0 +1,71 @@
+// console.log("1");
+// setTimeout(() => {
+//   console.log("2");
+// }, 1000);
+// console.log("3");
+
+// //동기적 콜백
+// function printNow(print) {
+//   print();
+// }
+// printNow(() => {
+//   console.log("콜백함수를 알아보쟈~~");
+// });
+
+// function printLater(print, time) {
+//   setTimeout(print, time);
+// }
+
+// printLater(() => {
+//   console.log("비동기적 콜백!");
+// }, 2000);
+
+// 콜백지옥 한번 당해볼텨?
+
+class UserStorage {
+  loginUser(id, password, onSuccess, onError) {
+    setTimeout(() => {
+      if (
+        (id === "moon" && password === "kyu") ||
+        (id === "kim" && password === "no")
+      ) {
+        onSuccess(id);
+      } else {
+        onError(new Error("not found"));
+      }
+    }, 1500);
+  }
+
+  getRoles(user, onSuccess, onError) {
+    setTimeout(() => {
+      if (user === "moon" || user === "kim") {
+        onSuccess({ name: "moon", role: "admin" });
+      } else {
+        onError(new Error("byebye"));
+      }
+    }, 1000);
+  }
+}
+
+const userStorage = new UserStorage();
+const id = prompt("enter your ID");
+const password = prompt("enter your password");
+
+userStorage.loginUser(
+  id,
+  password,
+  (user) => {
+    userStorage.getRoles(
+      user,
+      (userwithRole) => {
+        alert(`Hello ${userwithRole.name} as ${userwithRole.role}`);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  },
+  (error) => {
+    console.log(error);
+  }
+);
